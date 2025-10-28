c90bbce1b2bb47731d0e144a8d72b7b7f6f15540
Jesse Simpson <jesse.simpson@camunda.com>
2025-10-27 20:13:12 -0400

    fix: read the line on the template where a branch occurs
    Signed-off-by: Jesse Simpson <jesse.simpson@camunda.com>

--- internal/dlvcontroller/dlvcontroller.go
+++ internal/dlvcontroller/dlvcontroller.go
@@ -1,11 +1,14 @@
 package dlvcontroller
 
 import (
+	"bufio"
 	"context"
 	"fmt"
 	"github.com/go-delve/delve/service/api"
 	"github.com/go-delve/delve/service/rpc2"
+	"strconv"
 	"time"
+	"os"
 	"os/exec"
 )
 
@@ -63,6 +66,33 @@ func (r *RPCDlvController) Configure(ctx context.Context, client *rpc2.RPCClient
 	return nil
 }
 
+func ReadOneLine(fileName string, lineNumber int) (string, error) {
+	file, err := os.Open(fileName)
+	if err != nil {
+		fmt.Println("Error opening file:", err)
+		return "", err
+	}
+	defer file.Close()
+
+	scanner := bufio.NewScanner(file)
+	currentLine := 0
+	returned := ""
+
+	for scanner.Scan() {
+		if currentLine == lineNumber-1 {
+			returned = scanner.Text()
+			fmt.Println(returned)
+			break
+		}
+		currentLine++
+	}
+
+	if err := scanner.Err(); err != nil {
+		fmt.Println("Error reading file:", err)
+	}
+	return returned, nil
+}
+
 func (r *RPCDlvController) DisplayVars(client *rpc2.RPCClient) error {
 	loadConfig := api.LoadConfig{
 		FollowPointers:     true,
@@ -93,13 +123,21 @@ func (r *RPCDlvController) DisplayVars(client *rpc2.RPCClient) error {
 	fmt.Println("Name:", name.Value)
 	fmt.Println("Line:", line.Value)
 
-	vars, err := client.ListLocalVariables(api.EvalScope{GoroutineID: 1, Frame: 0}, loadConfig)
+	// read line number line.Value of file name.Value and print the line
+	lineNumber, err := strconv.Atoi(line.Value)
 	if err != nil {
-		return fmt.Errorf("Failed to list local variables: %w", err)
+		return fmt.Errorf("Failed to convert line number to int: %w", err)
 	}
-	for _, v := range vars {
-		fmt.Printf("%s: %s = %s\n", v.Name, v.Type, v.Value)
-	}
+	ReadOneLine(name.Value, lineNumber)
+
+
+//	vars, err := client.ListLocalVariables(api.EvalScope{GoroutineID: 1, Frame: 0}, loadConfig)
+//	if err != nil {
+//		return fmt.Errorf("Failed to list local variables: %w", err)
+//	}
+//	for _, v := range vars {
+//		fmt.Printf("%s: %s = %s\n", v.Name, v.Type, v.Value)
+//	}
 
 	return nil
 }
